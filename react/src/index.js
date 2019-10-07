7bcec8621b9bd62b9da2d96bd9bd13a313c4f6f6
scalerone <676119236@qq.com>
2019-10-07 16:10:27 +0800

    引入redux管理状态，创建store/reducer/action，和在index.js中引入react-redux

--- react/src/index.js
+++ react/src/index.js
@@ -9,10 +9,17 @@ import App from './App'
 import storageUtils from './utils/storageUtils'
 import memoryUtils from './utils/memoryUtils'
 
+import {Provider} from 'react-redux'
+import store from './redux/store'
+
 // 读取local中保存user, 保存到内存中
 const user = storageUtils.getUser()
 memoryUtils.user = user
-
-
 // 将App组件标签渲染到index页面的div上
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById('root'))
+
+// ReactDOM.render(<App />, document.getElementById('root'))
---
+++ react/src/redux/store.js
@@ -0,0 +1,9 @@
+
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import reducer from './reducer'
+
+// 向外默认暴露store
+export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
